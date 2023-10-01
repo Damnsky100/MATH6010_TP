2d48fc71f01b9c5597703ad9a17f72e774797c1d
xmarcoux <114787017+xmarcoux@users.noreply.github.com>
2023-10-01 15:13:15 -0400

    Add files via upload

--- Function/script1.R
+++ Function/script1.R
@@ -3,6 +3,7 @@ library("here")
 library("zoo")
 library("quantmod")
 library("TTR")
+library("openxlsx")
 source(here("Function", "f_get_vol_data.R"))
 source(here("Function", "f_clean_vol_data.R"))
 source(here("Function", "f_get_price_data.R"))
@@ -10,8 +11,19 @@ source(here("Function", "f_clean_price_data.R"))
 source(here("Function", "f_add_technicals.R"))
 source(here("Function", "f_compute_vol_ratios.R"))
 source(here("Function", "f_trading.R"))
+source(here("Function", "f_clean_data.R"))
+source(here("Function", "f_cluster_DOW.R"))
+source(here("Function", "f_extract_trades.R"))
 
-####### DATA ##########
+#### Initial Cluster #####
+# Retrieve the cluster which will be used
+f_clean_data_clustering()
+df_clusters_yearly <- f_cluster_DOW(10)
+
+# Unlist the dataframe to find all the unique stocks
+df_unlist <- as.vector(unlist(df_clusters_yearly))
+unique_stocks <- unique(df_unlist)
+
 
 # Volatility data
 df_vol <- f_get_vol_data()
@@ -34,18 +46,46 @@ names(list_prices) <- stocks_symbols
 
 # Determine all combos, keep them named
 stock_pairs <- combn(stocks_symbols, 2)
-  
+
+
+
+
+###### COMBOS  ######
+
+get_pairs <- function(stocks) {
+  combn(stocks, 2)
+}
+
+# Create a matrix to store pairs for each year
+all_pairs <- do.call(cbind, lapply(df_clusters_yearly, function(col) get_pairs(unique(na.omit(col)))))
+
+# Remove duplicate columns (unique pairs)
+unique_pairs_overall <- all_pairs[, !duplicated(t(all_pairs))]
+
+print(unique_pairs_overall)
+
+###########################3
+
+
 # create and save every vol ratio in a list
 list_ratios <- lapply(seq_len(ncol(stock_pairs)), function(i) f_compute_vol_ratios(stock_pairs[, i], list_vols, list_prices))
 # Rename every list element with the pair name
 names(list_ratios) <- apply(stock_pairs, 2, function(combos) paste(combos, collapse = "/"))
+
+# Clean the xts_objects within the list
+# Some stocks did not exits at certain point in time and it creates NAs
+list_ratios <- lapply(list_ratios, function(x) {
+  # For each xts object, remove rows where any column has NA
+  return(na.omit(x))
+})
+
 
 # Apply function to add technicals to every list element
 list_ratios_technicals <- lapply(list_ratios, function(i) f_add_technicals(i, 100, 2))
 
 ####
 # Chose a starting date 
-start_date <- as.Date("2001-01-01")
+start_date <- as.Date("2008-01-01")
 end_date <- as.Date("2022-12-30")
 
 # Function to crop every list element for what we need
@@ -57,23 +97,48 @@ crop_data <- function(xts_obj, start_date, end_date) {
 list_ratios_technicals <- lapply(list_ratios_technicals, crop_data, start_date = start_date, end_date = end_date)
 
 
-
-
 ######################################################################
 ### Trading
+ 
+list_trading <- lapply(list_ratios_technicals, function(i) f_trading(i, 10, df_clusters_yearly))
 
+# get trades
+list_trades <- lapply(list_trading, function(i) f_extract_trades(i))
+# filer the null
+list_df_trades <- list_trades[sapply(list_trades, function(df) !is.null(df))]
 
-list_trading <- lapply(list_ratios_technicals, function(i) f_trading(i, 10))
+# merge
+df_all_trades <- lapply(names(list_df_trades), function(df_name) {
+  df <- list_df_trades[[df_name]]
+  df$source <- df_name
+  return(df)
+})
+
+# Convert the list output from lapply back to a list (if it's not already)
+df_all_trades <- as.list(df_all_trades)
+
+# Combine the dataframes into one
+combined_df <- do.call(rbind, df_all_trades)
+#### clean up
+# Remove the 'source' column
+combined_df$source <- NULL
+# Rename 
+colnames(combined_df)[6] <- "return"
+colnames(combined_df)[7] <- "profitable"
+
+write.xlsx(combined_df, here("output", "trades_small.xlsx"))
 
 
 
 ##### PLOTS #####
-tmp <- list_trading[[3]]
+
+for (i in 1:length(list_trading)) {
+tmp <- list_trading[[i]]
 
 plot(index(tmp), coredata(tmp[,"equity_curve"]), type = 'l', col = 'black',
      main = "test",
      xlab = "Date", ylab = "Ratio")
-
+}
 
 ### plot to see
 plot(index(tmp), coredata(tmp[,"vol_ratio"]), type = 'l', col = 'black',
