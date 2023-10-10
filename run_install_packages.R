03700f7311b2ac4220f9c6c6c1da5074aa76ced0
xmarcoux <114787017+xmarcoux@users.noreply.github.com>
2023-10-10 16:24:51 -0400

    Add files via upload
    updated

--- run_install_packages.R
+++ run_install_packages.R
@@ -1,1 +1,5 @@
-install.packages(c("xts", "here", "zoo", "quantmod", "TTR", "openxlsx", "readxl", "writexl", "PerformanceAnalytics"))
+install.packages(c("xts", "here", "zoo", "quantmod", "TTR", "openxlsx", "readxl", "writexl",
+                   "PerformanceAnalytics", "lubridate", "stats", "caret", "glmnet", "Metrics",
+                   "R6", "ggplot2", "tidyverse"))
+
+
