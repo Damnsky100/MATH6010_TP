68b911786d6fa96b8e1c0e77976f72143543a7da
JAGMcgill <91499589+JAGMcgill@users.noreply.github.com>
2023-10-10 20:41:04 -0400

    Add files via upload
    Classification model added, needs to add feature to list_ratios_technicals and add trading logic

--- run_install_packages.R
+++ run_install_packages.R
@@ -1,5 +1,5 @@
 install.packages(c("xts", "here", "zoo", "quantmod", "TTR", "openxlsx", "readxl", "writexl",
                    "PerformanceAnalytics", "lubridate", "stats", "caret", "glmnet", "Metrics",
-                   "R6", "ggplot2", "tidyverse"))
+                   "R6", "ggplot2", "tidyverse", "randomForest"))
 
 
