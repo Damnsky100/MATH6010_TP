51f2a54daff9263fa4b3e0214a8d013e7aea46d0
xmarcoux <114787017+xmarcoux@users.noreply.github.com>
2023-10-13 18:42:08 -0400

    Add files via upload

--- run_outputs.R
+++ run_outputs.R
@@ -6,14 +6,16 @@
 equity_curves <- f_load_equity_curves()
 
 # Retrieve the trades
-all_trades <- f_load_trades()
+all_trades_list<- f_load_trades()
 
 # Generate the risk reports
-risk_metrics <- f_risk_metrics(equity_curves, all_trades)
+risk_metrics <- f_risk_metrics(equity_curves, all_trades_list, 0.025)
 print(risk_metrics["Portfolio_statistics"])
 print(risk_metrics["Trades_statistics"])
 
 # Plots
 
-f_call_plots(equity_curves)
+f_call_plots(equity_curves, all_trades_list)
+
+
 
